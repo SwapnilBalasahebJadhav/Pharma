4ea8372f0e29aae375f3254bc796775fd2c0734a
Swapnil Jadhav <swapjadhav35@gmail.com>
2025-12-29 23:42:25 +0530

    changes on backend cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 app.use(express.json());
 
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:["https://pharmasapp.netlify.app","http://localhost:3000"],
     credentials:true
 }))
 
