0c7a61e3c2836921110cce053de75647b00b7daf
Swapnil Jadhav <swapjadhav35@gmail.com>
2025-12-29 17:48:35 +0530

    Fix MySQL Railway connection config (add port support)

--- backend/src/config/connection.js
+++ backend/src/config/connection.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql2");
+require("dotenv").config();
+
+console.log("HOST:", process.env.DB_HOST);
+console.log("USER:", process.env.DB_USER);
+console.log("DB:", process.env.DB);
+console.log("PORT:", process.env.DB_PORT);
 
 const connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Swapnil@2002",
-  database: "figma",
+  host: process.env.DB_HOST,       
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  port: Number(process.env.DB_PORT), 
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = connection;
